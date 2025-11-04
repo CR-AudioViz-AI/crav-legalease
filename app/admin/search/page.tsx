ce59ffe83521a5ecb7135c73a6c311eefa7c00f8
Old <225056366+roy-henderson@users.noreply.github.com>
2025-11-04 16:02:17 -0500

    Add admin page: app/admin/search/page.tsx

---
+++ app/admin/search/page.tsx
@@ -0,0 +1,10 @@
+import AdminDashboardLayout from '@/components/admin/AdminDashboardLayout'
+import AdvancedSearch from '@/components/documents/AdvancedSearch'
+
+export default function SearchPage() {
+  return (
+    <AdminDashboardLayout>
+      <AdvancedSearch />
+    </AdminDashboardLayout>
+  )
+}
